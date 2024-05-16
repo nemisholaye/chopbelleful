ca83c381a1268a49f279aab30e62ba499bfbcadd
nemisholaye <nemisholaye@gmail.com>
2024-05-16 12:33:21 +0100

    first commit

--- src/components/Breakfast.jsx
+++ src/components/Breakfast.jsx
@@ -1,7 +1,11 @@
+import { useEffect } from "react";
 
 
 function Breakfast() {
 
+    useEffect(() => {
+        getPopular();
+    },[]);
     const getPopular = async () => {
         const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
         const data = await api.json();
